7626905a00b63ec31a0aea18661aeb4accd37f2a
satyambh779 <satyambh779@gmail.com>
2025-11-11 22:03:58 +0530

    Add files via upload

---
+++ assignment1/asssignment1.py
@@ -0,0 +1,28 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==================================================================== RESTART: C:/Users/SONAM/Desktop/tracker.2py.py ====================================================================
+Welcome, This Python tool helps you quickly track and analyze your daily calorie intake
+Enter the number of meals you want to add - 2
+Enter the name of your meal - rice
+Enter the amount of your calorie intake - 400
+Enter the name of your meal - rajma
+Enter the amount of your calorie intake - 300
+Total amount of your calorie intake is -  700.0
+Do you want to see the average calories per meal? (yes/no)? - yes
+Calculating your data...
+350.0
+Do you want to set your own daily calorie limit? (yes/no)? - yes
+Enter your daily calorie limit: 700
+Daily calorie limit Set to -  700.0
+Your calorie intake for today is in your daily limit.
+Meal name	Calories
+--------------------------
+rice	400.0
+rajma		300.0
+Total calorie	700.0
+Average calorie	350.0
+Do you want to save todays report (yes/no)? - yes
+Proceeding with action
+Enter the name as of the file to be created! - run1
+Enter todays date in any format - 11/11/2025
